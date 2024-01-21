8b33aaf6676abd84129ba6607e2864eaf3a239e0
Naoya-Yasuda <2301330039zz@cyber-u.ac.jp>
2024-01-21 21:26:56 +0900

    weatherアプリ作成

---
+++ python_practice/weather.py
@@ -0,0 +1,19 @@
+﻿import tkinter as tk
+import requests as rq
+
+url = 'https://weather.tsukumijima.net/api/forecast/city/'
+
+
+def 押された():  # ボタンがクリックされたら実行
+    r = rq.get(url + '170010')  # 170010
+    天気 = r.json()  # 辞書形式に変換
+    ラベル['text'] = 天気['forecasts'][1]['telop']
+
+
+w = tk.Tk()  # ウィンドウを作成
+ボタン = tk.Button(w, text='ボタン', command=押された)  # ボタンを作成
+ボタン.place(x=0, y=0)  # ボタンを配置
+ラベル = tk.Label(w, text='明日の天気')  # ラベルを作成
+ラベル.place(x=0, y=30)  # ラベルを配置
+
+w.mainloop()  # ウィンドウを表示
